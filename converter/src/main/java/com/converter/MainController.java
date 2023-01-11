19c1b8dfea1a9bd816000e15857e8f5a081c2284
uk <wjdtjr0307@gmail.com>
2023-01-11 23:12:03 +0900

    first

---
+++ converter/src/main/java/com/converter/MainController.java
@@ -0,0 +1,13 @@
+package com.converter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("main")
+    public String mainPage() {
+        return "/html/index.html";
+    }
+}
